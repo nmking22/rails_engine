acb03268a4b68085801203d5c4c016413a6c47d7
Nick King <nickmaxking@gmail.com>
2020-12-14 23:24:25 -0700

    Create model tests

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Merchant, type: :model do
+  describe "relationships" do
+    it {should have_many :items}
+    it {should have_many :invoices}
+    it {should have_many(:customers).through(:invoices)}
+  end
+end
