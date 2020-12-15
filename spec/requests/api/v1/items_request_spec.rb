74b7330b1851cdea701cbc2634639c7691dc8d40
Nick King <nickmaxking@gmail.com>
2020-12-15 15:09:21 -0700

    Adapt item request spec to use serializer

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -7,29 +7,28 @@ describe "Items API" do
     get '/api/v1/items'
 
     expect(response).to be_successful
-
     items = JSON.parse(response.body, symbolize_names: true)
 
-    expect(items.count).to eq(3)
+    expect(items[:data].count).to eq(3)
 
-    items.each do |item|
-      expect(item).to have_key(:name)
-      expect(item[:name]).to be_a(String)
+    items[:data].each do |item|
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
 
-      expect(item).to have_key(:description)
-      expect(item[:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
 
-      expect(item).to have_key(:unit_price)
-      expect(item[:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
 
-      expect(item).to have_key(:created_at)
-      expect(item[:created_at]).to be_a(String)
+      expect(item[:attributes]).to have_key(:created_at)
+      expect(item[:attributes][:created_at]).to be_a(String)
 
-      expect(item).to have_key(:updated_at)
-      expect(item[:updated_at]).to be_a(String)
+      expect(item[:attributes]).to have_key(:updated_at)
+      expect(item[:attributes][:updated_at]).to be_a(String)
 
-      expect(item).to have_key(:merchant_id)
-      expect(item[:merchant_id]).to be_a(Integer)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
     end
   end
 
@@ -42,22 +41,76 @@ describe "Items API" do
 
     expect(response).to be_successful
 
-    expect(item).to have_key(:name)
-    expect(item[:name]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
 
-    expect(item).to have_key(:description)
-    expect(item[:description]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
 
-    expect(item).to have_key(:unit_price)
-    expect(item[:unit_price]).to be_a(Float)
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
 
-    expect(item).to have_key(:created_at)
-    expect(item[:created_at]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:created_at)
+    expect(item[:data][:attributes][:created_at]).to be_a(String)
 
-    expect(item).to have_key(:updated_at)
-    expect(item[:updated_at]).to be_a(String)
+    expect(item[:data][:attributes]).to have_key(:updated_at)
+    expect(item[:data][:attributes][:updated_at]).to be_a(String)
 
-    expect(item).to have_key(:merchant_id)
-    expect(item[:merchant_id]).to be_a(Integer)
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+  end
+
+  it "can create a new item" do
+    merchant = Merchant.create!(
+      name: 'Karts and More',
+      created_at: '2012-03-27 14:53:59.000000',
+      updated_at: '2012-03-27 14:53:59.000000'
+    )
+    item_params = ({
+                    name: 'Green Shell',
+                    description: 'Shoots straight',
+                    unit_price: 5.55,
+                    created_at: '2012-03-27 14:53:59.000000',
+                    updated_at: '2012-03-27 14:53:59.000000',
+                    merchant_id: merchant.id
+                  })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.created_at).to eq(item_params[:created_at])
+    expect(created_item.updated_at).to eq(item_params[:updated_at])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it "can update an existing item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Blue Shell" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Blue Shell")
+  end
+
+  it "can destroy an item" do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
