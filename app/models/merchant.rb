80cf39425e4a24663f7b6c33faf97baf4053667c
Nick King <nickmaxking@gmail.com>
2020-12-15 15:24:46 -0700

    Add attribute validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
+  validates_presence_of :name,
+                        :created_at,
+                        :updated_at
+
   has_many :items
   has_many :invoices
   has_many :customers, through: :invoices
