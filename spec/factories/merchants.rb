ace82b196066d44173b245b4139129e0800caeb7
Nick King <nickmaxking@gmail.com>
2020-12-15 15:25:13 -0700

    Add items trait

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -4,6 +4,12 @@ FactoryBot.define do
       name { Faker::Company.name }
       created_at { "MyString" }
       updated_at { "MyString" }
+
+      trait :with_items do
+        after :create do |merchant|
+          create_list(:item, 2, merchant: merchant)
+        end
+      end
     end
   end
 end
