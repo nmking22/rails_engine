d77066b6d1b0907587b3551429be6570165b2771
Nick King <nickmaxking@gmail.com>
2020-12-15 15:09:58 -0700

    Adapt item controller to use serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,27 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params[:id])
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params))
+  end
+
+  def update
+    render json: ItemSerializer.new(Item.update(params[:id], item_params))
+  end
+
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :created_at, :updated_at, :merchant_id)
+    end
 end
