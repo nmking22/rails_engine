1a3203357327765cd83f18572f771b2144842f9f
Nick King <nickmaxking@gmail.com>
2020-12-15 22:02:18 -0700

    Add routes and controller actions for item search endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/merchants/find', to: 'merchants/search#show'
+      get '/items/find', to: 'items/search#show'
       get '/merchants/find_all', to: 'merchants/search#index'
+      get '/items/find_all', to: 'items/search#index'
       get '/merchants/:id/items', to: 'merchants/items#index'
       get '/items/:id/merchants', to: 'items/merchants#show'
       resources :merchants
