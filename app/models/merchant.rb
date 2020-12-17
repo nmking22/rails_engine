60f92864f922a87c3f06e14b27276ce7d046b59f
Nick King <nickmaxking@gmail.com>
2020-12-16 20:18:00 -0700

    Add most_items and most_revenue class methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,24 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :customers, through: :invoices
+
+  def self.most_revenue(quantity = 1)
+    Merchant.select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+    .joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: "success"})
+    .where(invoices: {status: "shipped"})
+    .group(:id)
+    .order("revenue DESC")
+    .limit(quantity)
+  end
+
+  def self.most_items(quantity = 1)
+    Merchant.select("merchants.*, SUM(invoice_items.quantity) AS total_quantity")
+    .joins(invoices: [:invoice_items, :transactions])
+    .where(transactions: {result: "success"})
+    .where(invoices: {status: "shipped"})
+    .group(:id)
+    .order("total_quantity DESC")
+    .limit(quantity)
+  end
 end
