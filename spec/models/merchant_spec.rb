710129591bcab1c29028364e5a44fbcce0b769e3
Nick King <nickmaxking@gmail.com>
2020-12-17 15:45:22 -0700

    Add test for revenue instance method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -118,4 +118,96 @@ describe Merchant, type: :model do
       expect(Merchant.most_revenue).to eq([@merchant_1])
     end
   end
+
+  describe 'instance methods' do
+    it '#revenue' do
+      merchant = Merchant.create(name: "Bob's Burgers")
+      item_1 = Item.create(
+        name: 'Burger',
+        description: 'Good',
+        unit_price: 5.00,
+        merchant: merchant
+      )
+      item_2 = Item.create(
+        name: 'Pizza',
+        description: 'Evil',
+        unit_price: 10.00,
+        merchant: merchant
+      )
+      customer = Customer.create(
+        first_name: 'John Michael',
+        last_name: 'Vincent'
+      )
+      invoice_1 = Invoice.create(
+        customer: customer,
+        merchant: merchant,
+        status: 'shipped'
+      )
+      invoice_2 = Invoice.create(
+        customer: customer,
+        merchant: merchant,
+        status: 'shipped'
+      )
+      invoice_3 = Invoice.create(
+        customer: customer,
+        merchant: merchant,
+        status: 'packaged'
+      )
+      invoice_4 = Invoice.create(
+        customer: customer,
+        merchant: merchant,
+        status: 'shipped'
+      )
+      invoice_item_1 = InvoiceItem.create(
+        item: item_1,
+        invoice: invoice_1,
+        quantity: 500,
+        unit_price: 5.00
+      )
+      invoice_item_2 = InvoiceItem.create(
+        item: item_2,
+        invoice: invoice_2,
+        quantity: 50,
+        unit_price: 10.00
+      )
+      invoice_item_3 = InvoiceItem.create(
+        item: item_1,
+        invoice: invoice_3,
+        quantity: 99999,
+        unit_price: 50.00
+      )
+      invoice_item_4 = InvoiceItem.create(
+        item: item_2,
+        invoice: invoice_4,
+        quantity: 99999,
+        unit_price: 50.00
+      )
+      transaction_1 = Transaction.create(
+        invoice: invoice_1,
+        credit_card_number: 12345,
+        credit_card_expiration_date: 04/23,
+        result: 'success'
+      )
+      transaction_2 = Transaction.create(
+        invoice: invoice_2,
+        credit_card_number: 12345,
+        credit_card_expiration_date: 04/23,
+        result: 'success'
+      )
+      transaction_3 = Transaction.create(
+        invoice: invoice_3,
+        credit_card_number: 12345,
+        credit_card_expiration_date: 04/23,
+        result: 'success'
+      )
+      transaction_4 = Transaction.create(
+        invoice: invoice_4,
+        credit_card_number: 12345,
+        credit_card_expiration_date: 04/23,
+        result: 'failed'
+      )
+
+      expect(merchant.revenue).to eq(3000.00)
+    end
+  end
 end
